03873e3b7fc89ca4f2d26b627c80de883255e1ed
biancahng <57821174+biancahng@users.noreply.github.com>
2021-05-25 01:19:37 -0300

    Update EstimandoPI.cs

--- EstimandoPI.cs
+++ EstimandoPI.cs
@@ -1,10 +1,28 @@
-﻿using System;
+using System;
 
-public class Class1
+class EstimandoPI
 {
-	public Class1()
+	static void Main()
 	{
-		
+		Random aleatorio = new Random();
+		double n = 10000;
+		double j = 0;
+		double pi, x, y;
 
+		// Proporção = área do círculo / área do quadrado
+		// P = pi * r^2 / l^2
+
+		for (int i = 0; i < n; i++)
+		{
+			x = aleatorio.NextDouble() * (1 - (-1)) + (-1);
+			y = aleatorio.NextDouble() * (1 - (-1)) + (-1);
+
+			if ((Math.Pow(x, 2) + Math.Pow(y, 2)) <= 1)
+				j++;
+		}
+
+		pi = j / n * 4;
+
+		Console.WriteLine("PI vale " + pi);
 	}
 }
